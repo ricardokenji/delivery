6f12f7188fcd8d84cb0f382304849d8d1e645dd1
Ricardo Kenji <ricardo.kenji@lar.app>
2019-12-08 19:48:54 -0300

    improve order parser test
    One assertion per test

--- spec/services/order_parser/eats_order_parser_spec.rb
+++ spec/services/order_parser/eats_order_parser_spec.rb
@@ -1,49 +1,135 @@
 RSpec.describe OrderParser::EatsOrderParser do
   describe "Post parse" do
-    it "Should Parse" do
+    before(:each) do
       file = file_fixture("payload.json").read
-      json = JSON.parse(file)
-      json_address = json['shipping']['receiver_address']
+      @json = JSON.parse(file)
+      @json_address = @json['shipping']['receiver_address']
+      @json_item = @json['order_items'].first
+      @json_payment = @json['payments'].first
+      @json_customer = @json['buyer']
 
       order_parser = OrderParser::EatsOrderParser.new
-      order = order_parser.parse(json)
+      @order = order_parser.parse(@json)
+    end
 
-      expect(order.external_code).to eq(json['id'].to_s)
-      expect(order.store_id).to eq(json['store_id'].to_s)
-      expect(order.sub_total).to eq(json['total_amount'])
-      expect(order.delivery_fee).to eq(json['total_shipping'])
-      expect(order.total).to eq(json['total_amount_with_shipping'])
-      expect(order.country).to eq(json_address['country']['id'])
-      expect(order.state).to eq(json_address['state']['name'])
-      expect(order.city).to eq(json_address['city']['name'])
-      expect(order.district).to eq(json_address['neighborhood']['name'])
-      expect(order.street).to eq(json_address['address_line'])
-      expect(order.complement).to eq(json_address['comment'])
-      expect(order.latitude).to eq(json_address['latitude'])
-      expect(order.longitude).to eq(json_address['longitude'])
-      expect(order.dt_order_create).to eq(json['date_created'])
-      expect(order.postal_code).to eq(json_address['zip_code'])
-      expect(order.number).to eq(json_address['street_number'])
-      expect(order.status).to eq('pending')
+    it "Should have order.external_code that matches" do
+      expect(@order.external_code).to eq(@json['id'].to_s)
+    end
+
+    it "Should have order.store_id that matches" do
+      expect(@order.store_id).to eq(@json['store_id'].to_s)
+    end
+
+    it "Should have order.sub_total that matches" do
+      expect(@order.sub_total).to eq(@json['total_amount'])
+    end
+
+    it "Should have order.delivery_fee that matches" do
+      expect(@order.delivery_fee).to eq(@json['total_shipping'])
+    end
+
+    it "Should have order.total that matches" do
+      expect(@order.total).to eq(@json['total_amount_with_shipping'])
+    end
+
+    it "Should have order.country that matches" do
+      expect(@order.country).to eq(@json_address['country']['id'])
+    end
+
+    it "Should have order.state that matches" do
+      expect(@order.state).to eq(@json_address['state']['name'])
+    end
+
+    it "Should have order.city that matches" do
+      expect(@order.city).to eq(@json_address['city']['name'])
+    end
+
+    it "Should have order.district that matches" do
+      expect(@order.district).to eq(@json_address['neighborhood']['name'])
+    end
+
+    it "Should have order.street that matches" do
+      expect(@order.street).to eq(@json_address['address_line'])
+    end
+
+    it "Should have order.complement that matches" do
+      expect(@order.complement).to eq(@json_address['comment'])
+    end
+
+    it "Should have order.latitude that matches" do
+      expect(@order.latitude).to eq(@json_address['latitude'])
+    end
+
+    it "Should have order.longitude that matches" do
+      expect(@order.longitude).to eq(@json_address['longitude'])
+    end
+
+    it "Should have order.dt_order_create that matches" do
+      expect(@order.dt_order_create).to eq(@json['date_created'])
+    end
+
+    it "Should have order.postal_code that matches" do
+      expect(@order.postal_code).to eq(@json_address['zip_code'])
+    end
+
+    it "Should have order.number that matches" do
+      expect(@order.number).to eq(@json_address['street_number'])
+    end
+
+    it "Should have order.status that matches" do
+      expect(@order.status).to eq('pending')
+    end
      
-      json_item = json['order_items'].first
-      expect(order.items.first.external_code).to eq(json_item['item']['id'])
-      expect(order.items.first.name).to eq(json_item['item']['title'])
-      expect(order.items.first.price).to eq(json_item['unit_price'])
-      expect(order.items.first.quantity).to eq(json_item['quantity'])
-      expect(order.items.first.total).to eq(json_item['full_unit_price'])
+    it "Should have items.external_code that matches" do
+      expect(@order.items.first.external_code).to eq(@json_item['item']['id'])
+    end
 
-      json_customer = json['buyer']
-      expect(order.customer.external_code).to eq(json_customer['id'].to_s)
-      expect(order.customer.first_name).to eq(json_customer['first_name'])
-      expect(order.customer.last_name).to eq(json_customer['last_name'])
-      expect(order.customer.email).to eq(json_customer['email'])
-      expect(order.customer.phone_area_code).to eq(json_customer['phone']['area_code'].to_s)
-      expect(order.customer.phone_number).to eq(json_customer['phone']['number'])
+    it "Should have items.name that matches" do
+      expect(@order.items.first.name).to eq(@json_item['item']['title'])
+    end
 
-      json_payment = json['payments'].first
-      expect(order.payments.first.type).to eq(json_payment['payment_type'])
-      expect(order.payments.first.value).to eq(json_payment['installment_amount'])
+    it "Should have items.price that matches" do
+      expect(@order.items.first.price).to eq(@json_item['unit_price'])
+    end
+
+    it "Should have items.quantity that matches" do
+      expect(@order.items.first.quantity).to eq(@json_item['quantity'])
+    end
+
+    it "Should have items.total that matches" do
+      expect(@order.items.first.total).to eq(@json_item['full_unit_price'])
+    end
+
+    it "Should have customer.storexternal_codee_id that matches" do
+      expect(@order.customer.external_code).to eq(@json_customer['id'].to_s)
+    end
+
+    it "Should have customer.first_name that matches" do
+      expect(@order.customer.first_name).to eq(@json_customer['first_name'])
+    end
+
+    it "Should have customer.last_name that matches" do
+      expect(@order.customer.last_name).to eq(@json_customer['last_name'])
+    end
+
+    it "Should have customer.email that matches" do
+      expect(@order.customer.email).to eq(@json_customer['email'])
+    end
+
+    it "Should have customer.phone_area_code that matches" do
+      expect(@order.customer.phone_area_code).to eq(@json_customer['phone']['area_code'].to_s)
+    end
+
+    it "Should have customer.phone_number that matches" do
+      expect(@order.customer.phone_number).to eq(@json_customer['phone']['number'])
+    end
+
+    it "Should have payments.type that matches" do
+      expect(@order.payments.first.type).to eq(@json_payment['payment_type'])
+    end
+
+    it "Should have payments.value that matches" do
+      expect(@order.payments.first.value).to eq(@json_payment['installment_amount'])
     end
   end 
 end
