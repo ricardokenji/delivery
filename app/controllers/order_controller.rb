cebea8b7ea9bb1dbbdd2d55b79a576116206fc64
Ricardo Kenji <ricardo.kenji@lar.app>
2019-12-08 13:04:45 -0300

    Add call to service and parser to controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,12 +1,23 @@
-class OrderController < ApplicationController
+class OrderController < ApplicationController 
   def create
     payload = get_payload
-    order = OrderParser.call(payload)
+
+    order_parser = get_parser_for_store_id(payload['store_id'])
+    ProcessOrder.call(order_parser.parse(payload))
+    
     render :plain => order, status: 200, content_type: 'application/json'
   end
 
   private
   def get_payload
     file = File.read(File.join(Rails.root, 'payload.json'))
+    JSON.parse(file)
+  end
+
+  def get_parser_for_store_id(store_id)
+    if(store_id == 282)
+      return OrderParser::EatsOrderParser.new
+    end
+    raise "Unsupported store"
   end
 end
